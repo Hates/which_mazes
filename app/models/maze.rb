3d75d1ff4ebfb445dccf4ad6d522493172e87685
Richard Hart <richard@ur-ban.com>
2015-11-11 14:12:26 +0000

    Add blank maze generator

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -3,4 +3,23 @@ class Maze < ActiveRecord::Base
   has_many :maze_cells
   has_many :cells, through: :maze_cells
 
+  # Create empty grid of cells for future use.
+  def generate_base_cells
+    width.times do |x|
+      height.times do |y|
+        maze_cells << MazeCell.build_cell(self, x, y)
+      end
+    end
+
+    save!
+  end
+
+  def columns
+    maze_cells.group_by(&:x)
+  end
+
+  def rows
+    maze_cells.group_by(&:y)
+  end
+
 end
--- spec/models/maze_spec.rb
+++ spec/models/maze_spec.rb
@@ -1,4 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Maze, type: :model do
+
+  let(:width) { 10 }
+  let(:height) { 10 }
+
+  subject { Maze.new width: width, height: height }
+
+  describe "#generate_base_cells" do
+
+    it "generates the right number of maze cells" do
+      subject.generate_base_cells
+
+      expect(subject.maze_cells.size).to equal(width * height)
+    end
+
+    it "generates the right number of cells" do
+      subject.generate_base_cells
+
+      expect(subject.cells.size).to equal(width * height)
+    end
+
+  end
+
+  describe "#rows" do
+
+    before { subject.generate_base_cells }
+
+    it "has the right number of rows" do
+      expect(subject.rows.size).to equal(height)
+    end
+
+  end
+
+  describe "#columns" do
+
+    before { subject.generate_base_cells }
+
+    it "has the right number of columns" do
+      expect(subject.columns.size).to equal(width)
+    end
+
+  end
+
 end
